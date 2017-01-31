6c245702c8a2fd051fc5a75193b7b9864b66c3fb
Phil Lamplugh <phillip.lamplugh@gmail.com>
2017-01-30 20:00:38 -0800

    working google

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('profile');
+  const user = req.session.user;
+  if (!user) return res.redirect('/');
+  res.render('profile', {user});
+});
+
+router.get('/me', (req, res, next) => {
+  const url = 'https://www.googleapis.com/plus/v1/people/me';
+  const access_token = req.session.access_token;
+  if (!access_token) return res.redirect('/');
+  const options = {
+    method: 'GET',
+    url,
+    headers: { 'Authorization' : `Bearer ${access_token}`}
+  }
+  request(options, (err, response, body) => {
+    const user = JSON.parse(body);
+    req.session.user = user;
+    return res.redirect('/profile');
+  })
 });
 
 module.exports = router;
